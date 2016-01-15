6b8777c444070904c28df01ae5ffd0865889bc19
FarmorsBetong <robin19972012@hotmail.se>
2016-01-15 10:30:30 +0100

    FAN
    som fan

---
+++ js/costum-fade.js
@@ -0,0 +1,22 @@
+/*global $ */
+
+/*$(document).ready(function(){
+    $(".btn1").click(function(){
+        $("#div").fadeOut()
+    });
+    $(".btn2").click(function(){
+        $("#div").fadeIn();
+    });
+});*/
+
+function KnappOpacity() {
+  "use strict";
+  document.getElementById("div").style.transition = "opacity 1s linear";
+  document.getElementById("div").style.opacity = 1;
+  document.getElementById('farmor').style.transition = "opacity 1s linear";
+  document.getElementById('farmor').style.opacity = 1;
+}
+
+function testa() {
+  
+}
--- php5/php5ScriptMail.php
+++ php5/php5ScriptMail.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
 
 <?php
 $firstname = $_POST['fnamn'];
 $Lastname = $_POST['enamn'];
 $Email = $_POST['email'];
 $Message = $_POST['comment'];
+$Price = $_POST['price'];
 
 $Till = "Robin19972012@hotmail.se";
 $Subject = "Nytt Meddelande";
 
-mail ($Till, $Subject, $Message, "From: " . $firstname .  $Lastname);
-echo "Ditt meddelande har blivit skickad";
+mail ($Till, $Subject, $Message, "Pris " . $Price, "From: " . $firstname .  $Lastname);
+if(mail){
+  echo "Ditt meddelande har blivit skickad";
+}
+
  ?>
-</body>
-</html>
